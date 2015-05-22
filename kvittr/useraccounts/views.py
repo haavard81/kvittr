17460ae3a8ca635096b1ce1a2ad64ceaa18ba21f
haavard81 <haavard.ramstad@gmail.com>
2015-05-22 23:49:02 +0200

    Added view with function user_login

--- kvittr/useraccounts/views.py
+++ kvittr/useraccounts/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
-# Create your views here.
+def user_login(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # redirect to a success page.
+            return redirect('post_listing')
+        else:
+            # render login again, but display error message
+            context['login_failed'] = True
+    # request.method == 'GET':
+    return render(request, 'useraccounts/login.html', context)
