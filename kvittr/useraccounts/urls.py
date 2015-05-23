16c0ddc7f7e12bc097ad904e4812c42ae26d4939
haavard81 <haavard.ramstad@gmail.com>
2015-05-23 16:01:05 +0200

    Urlpattern added for user_register view

--- kvittr/useraccounts/urls.py
+++ kvittr/useraccounts/urls.py
@@ -4,4 +4,5 @@ from useraccounts import views
 urlpatterns = patterns('',
     url(r'^login$', views.user_login, name='user_login'),
     url(r'^logout$', views.user_logout, name='user_logout'),
+    url(r'^register$', views.user_register, name='user_register'),
 )
