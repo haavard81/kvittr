a1c2637090607d4ed009eadcf00014336a65f42e
haavard81 <haavard.ramstad@gmail.com>
2015-05-23 22:11:56 +0200

    Added urlpattern for post_details to posts urls.py

--- kvittr/posts/urls.py
+++ kvittr/posts/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from posts import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.post_listing, name='post_listing'),        
+    url(r'^$', views.post_listing, name='post_listing'),
+    url(r'^(\d+)/$', views.post_details, name='post_details'),        
 )
