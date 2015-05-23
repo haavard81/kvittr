dd75659c1888153e5c5517d63315ceba626c040a
haavard81 <haavard.ramstad@gmail.com>
2015-05-23 02:37:51 +0200

    created urls file for useraccounts. added login and logout url

---
+++ kvittr/useraccounts/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+from useraccounts import views
+
+urlpatterns = patterns('',
+    url(r'^login$', views.user_login, name='user_login'),
+    url(r'^logout$', views.user_logout, name='user_logout'),
+)
