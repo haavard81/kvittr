e881c24e3f2f7d0e88a388188d1380692290a5b0
haavard81 <haavard.ramstad@gmail.com>
2015-05-23 21:16:23 +0200

    Created view for saving post to db and showing posts on post_listing.html with pagination.

--- kvittr/posts/views.py
+++ kvittr/posts/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.utils import timezone
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from posts.models import Post
+
+def post_listing(request):
+
+    if request.method == 'POST':
+        new_post = request.POST.get('new_post')
+        if new_post:
+            post = Post()
+            post.message = new_post
+            post.created_datetime = timezone.now()
+            post.created_by = request.user
+            post.save()
+
+    posts = Post.objects.all()
+    #posts = Post.objects.filter().order_by('-likes')
+    page = request.GET.get('page')
+    paginator = Paginator(posts, 5)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    context = {
+        'posts': posts,
+        }
+    return render(request, 'posts/post_listing.html', context)
