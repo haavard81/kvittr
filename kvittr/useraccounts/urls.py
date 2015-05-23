61a11bdb34feb40a85a5acea100d5b682bdcdd5d
haavard81 <haavard.ramstad@gmail.com>
2015-05-23 17:22:01 +0200

    Added urlpattern for user_edit view

--- kvittr/useraccounts/urls.py
+++ kvittr/useraccounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     url(r'^login$', views.user_login, name='user_login'),
     url(r'^logout$', views.user_logout, name='user_logout'),
     url(r'^register$', views.user_register, name='user_register'),
+    url(r'^edit$', views.user_edit, name='user_edit'),
 )
