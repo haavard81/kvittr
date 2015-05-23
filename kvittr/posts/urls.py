d48a7370a5d7fce9e55ba28f295c32be06648a44
haavard81 <haavard.ramstad@gmail.com>
2015-05-24 00:00:16 +0200

    Added urlpattern for post_add_likes on posts urls

--- kvittr/posts/urls.py
+++ kvittr/posts/urls.py
@@ -4,5 +4,6 @@ from posts import views
 
 urlpatterns = patterns('',
     url(r'^$', views.post_listing, name='post_listing'),
-    url(r'^(\d+)/$', views.post_details, name='post_details'),        
+    url(r'^(\d+)/$', views.post_details, name='post_details'),
+    url(r'^(\d+)/add_likes$', views.post_add_likes, name='post_add_likes'),         
 )
