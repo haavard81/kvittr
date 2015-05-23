edac5fb512ba0cad593d4051abc2159f0b53bba8
haavard81 <haavard.ramstad@gmail.com>
2015-05-23 18:46:03 +0200

    Added urlpattern for posts in kvittr urls.py

--- kvittr/kvittr/urls.py
+++ kvittr/kvittr/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('theme.urls')),
     url(r'^useraccounts/', include('useraccounts.urls')),
+    url(r'^posts/', include('posts.urls')),
 ]
