3b4d023d63edbddb820a72f98d4936dd39e0345d
haavard81 <haavard.ramstad@gmail.com>
2015-05-23 20:53:13 +0200

    Created class Post in posts models

--- kvittr/posts/models.py
+++ kvittr/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    message = models.TextField()
+    created_datetime = models.DateTimeField()
+    created_by = models.ForeignKey(User, related_name='created_posts')
+    likes = models.PositiveIntegerField(default=0)
+
+    def __unicode__(self):
+        return u'%s' % self.message
+
+    class Meta:
+        ordering = ['-id']
